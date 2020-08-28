ea22cbb05a0dc3c44e08de767789be04b08bcf65
Stefan Giba <stefang867@gmail.com>
2020-08-28 20:17:38 +0300

    almost ready

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,9 +2,9 @@ import { nanoid } from "nanoid";
 
 // HEAD DATA
 export const headData = {
-  title: "", // e.g: 'Name | Developer'
+  title: "Stefan Giba", // e.g: 'Name | Developer'
   lang: "", // e.g: en, es, fr, jp
-  description: "", // e.g: Welcome to my website
+  description: "Welcome to my portfolio website", // e.g: Welcome to my website
 };
 
 // HERO DATA
